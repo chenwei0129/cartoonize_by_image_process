bcb7fb27b41b117e9175e9fcb044b523b2d2c9fd
chenwei0129 <125378013+chenwei0129@users.noreply.github.com>
2023-07-26 16:04:06 +0800

    Update cartoonize.py

--- cartoonize.py
+++ cartoonize.py
@@ -1,13 +1,13 @@
 import cv2
 import numpy as np
 
-original_image = cv2.imread('test2.png')
+original_image = cv2.imread('0.jpg')
 
 gray_image = cv2.cvtColor(original_image, cv2.COLOR_BGR2GRAY)
 
 median_filtered_image = cv2.medianBlur(gray_image, 5)
 
-edges = cv2.Canny(median_filtered_image, 50, 100)
+edges = cv2.Canny(median_filtered_image, 60, 120)
 
 kernel = np.ones((2, 2), np.uint8)
 
@@ -21,43 +21,55 @@ blue_smoothed_image_Q = cv2.blur(blue_channel, (3, 3))
 green_smoothed_image_Q = cv2.blur(green_channel, (3, 3))
 red_smoothed_image_Q = cv2.blur(red_channel, (3, 3))
 
-mask1 = (blue_smoothed_image_Q >= 0) & (blue_smoothed_image_Q <= 39)
+mask1 = (blue_smoothed_image_Q >= 0) & (blue_smoothed_image_Q <= 31)
 blue_smoothed_image_Q[mask1] = 0
-mask2 = (blue_smoothed_image_Q >= 40) & (blue_smoothed_image_Q <= 79)
-blue_smoothed_image_Q[mask2] = 55
-mask3 = (blue_smoothed_image_Q >= 80) & (blue_smoothed_image_Q <= 119)
-blue_smoothed_image_Q[mask3] = 110
-mask4 = (blue_smoothed_image_Q >= 120) & (blue_smoothed_image_Q <= 179)
-blue_smoothed_image_Q[mask4] = 165
-mask5 = (blue_smoothed_image_Q >= 180) & (blue_smoothed_image_Q <= 239)
-blue_smoothed_image_Q[mask5] = 220
-mask6 = (blue_smoothed_image_Q >= 240) & (blue_smoothed_image_Q <= 255)
+mask2 = (blue_smoothed_image_Q >= 32) & (blue_smoothed_image_Q <= 63)
+blue_smoothed_image_Q[mask2] = 47
+mask3 = (blue_smoothed_image_Q >= 64) & (blue_smoothed_image_Q <= 95)
+blue_smoothed_image_Q[mask3] = 80
+mask4 = (blue_smoothed_image_Q >= 96) & (blue_smoothed_image_Q <= 127)
+blue_smoothed_image_Q[mask4] = 112
+mask5 = (blue_smoothed_image_Q >= 128) & (blue_smoothed_image_Q <= 159)
+blue_smoothed_image_Q[mask5] = 144
+mask6 = (blue_smoothed_image_Q >= 160) & (blue_smoothed_image_Q <= 191)
+blue_smoothed_image_Q[mask6] = 176
+mask6 = (blue_smoothed_image_Q >= 192) & (blue_smoothed_image_Q <= 223)
+blue_smoothed_image_Q[mask6] = 208
+mask6 = (blue_smoothed_image_Q >= 224) & (blue_smoothed_image_Q <= 255)
 blue_smoothed_image_Q[mask6] = 255
 
-mask1 = (green_smoothed_image_Q >= 0) & (green_smoothed_image_Q <= 39)
+mask1 = (green_smoothed_image_Q >= 0) & (green_smoothed_image_Q <= 31)
 green_smoothed_image_Q[mask1] = 0
-mask2 = (green_smoothed_image_Q >= 40) & (green_smoothed_image_Q <= 79)
-green_smoothed_image_Q[mask2] = 55
-mask3 = (green_smoothed_image_Q >= 80) & (green_smoothed_image_Q <= 119)
-green_smoothed_image_Q[mask3] = 110
-mask4 = (green_smoothed_image_Q >= 120) & (green_smoothed_image_Q <= 179)
-green_smoothed_image_Q[mask4] = 165
-mask5 = (green_smoothed_image_Q >= 180) & (green_smoothed_image_Q <= 239)
-green_smoothed_image_Q[mask5] = 220
-mask6 = (green_smoothed_image_Q >= 240) & (green_smoothed_image_Q <= 255)
+mask2 = (green_smoothed_image_Q >= 32) & (green_smoothed_image_Q <= 63)
+green_smoothed_image_Q[mask2] = 47
+mask3 = (green_smoothed_image_Q >= 64) & (green_smoothed_image_Q <= 95)
+green_smoothed_image_Q[mask3] = 80
+mask4 = (green_smoothed_image_Q >= 96) & (green_smoothed_image_Q <= 127)
+green_smoothed_image_Q[mask4] = 112
+mask5 = (green_smoothed_image_Q >= 128) & (green_smoothed_image_Q <= 159)
+green_smoothed_image_Q[mask5] = 144
+mask6 = (green_smoothed_image_Q >= 160) & (green_smoothed_image_Q <= 191)
+green_smoothed_image_Q[mask6] = 176
+mask6 = (green_smoothed_image_Q >= 192) & (green_smoothed_image_Q <= 223)
+green_smoothed_image_Q[mask6] = 208
+mask6 = (green_smoothed_image_Q >= 224) & (green_smoothed_image_Q <= 255)
 green_smoothed_image_Q[mask6] = 255
 
-mask1 = (red_smoothed_image_Q >= 0) & (red_smoothed_image_Q <= 39)
+mask1 = (red_smoothed_image_Q >= 0) & (red_smoothed_image_Q <= 31)
 red_smoothed_image_Q[mask1] = 0
-mask2 = (red_smoothed_image_Q >= 40) & (red_smoothed_image_Q <= 79)
-red_smoothed_image_Q[mask2] = 55
-mask3 = (red_smoothed_image_Q >= 80) & (red_smoothed_image_Q <= 119)
-red_smoothed_image_Q[mask3] = 110
-mask4 = (red_smoothed_image_Q >= 120) & (red_smoothed_image_Q <= 179)
-red_smoothed_image_Q[mask4] = 165
-mask5 = (red_smoothed_image_Q >= 180) & (red_smoothed_image_Q <= 239)
-red_smoothed_image_Q[mask5] = 220
-mask6 = (red_smoothed_image_Q >= 240) & (red_smoothed_image_Q <= 255)
+mask2 = (red_smoothed_image_Q >= 32) & (red_smoothed_image_Q <= 63)
+red_smoothed_image_Q[mask2] = 47
+mask3 = (red_smoothed_image_Q >= 64) & (red_smoothed_image_Q <= 95)
+red_smoothed_image_Q[mask3] = 80
+mask4 = (red_smoothed_image_Q >= 96) & (red_smoothed_image_Q <= 127)
+red_smoothed_image_Q[mask4] = 112
+mask5 = (red_smoothed_image_Q >= 128) & (red_smoothed_image_Q <= 159)
+red_smoothed_image_Q[mask5] = 144
+mask6 = (red_smoothed_image_Q >= 160) & (red_smoothed_image_Q <= 191)
+red_smoothed_image_Q[mask6] = 176
+mask6 = (red_smoothed_image_Q >= 192) & (red_smoothed_image_Q <= 223)
+red_smoothed_image_Q[mask6] = 208
+mask6 = (red_smoothed_image_Q >= 224) & (red_smoothed_image_Q <= 255)
 red_smoothed_image_Q[mask6] = 255
 
 color_image_Q = np.dstack((blue_smoothed_image_Q, green_smoothed_image_Q, red_smoothed_image_Q))
